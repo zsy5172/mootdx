3b8366d548ad99bf4eec152af1840d798435eb9e
root <root@DESKTOP-1B20OR6.localdomain>
2026-07-21 00:32:01 +0800

    fix: include trading session start boundaries

--- mootdx_next/session.py
+++ mootdx_next/session.py
@@ -7,10 +7,10 @@ def is_trading_session(now: datetime | None = None) -> bool:
     current = now or datetime.now()
     minute_of_day = current.hour * 60 + current.minute
 
-    if 9 * 60 + 30 < minute_of_day < 11 * 60 + 30:
+    if 9 * 60 + 30 <= minute_of_day < 11 * 60 + 30:
         return True
 
-    if 13 * 60 < minute_of_day < 15 * 60:
+    if 13 * 60 <= minute_of_day < 15 * 60:
         return True
 
     return False
